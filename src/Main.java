25d76c74908e8f91c1e14e09b2da3fbd94c7023f
SemenSMR <svorovali@mail.ru>
2023-03-29 15:19:06 +0300

    adding files

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year, day ,days = 0 ,count = 0;
+        while (true) {
+            System.out.print("Введите год: ");
+            year = scanner.nextInt();
+            System.out.print("Введите количество дней: ");
+            day = scanner.nextInt();
+              if (year % 400 == 0 || year % 400 != 0 && year % 100 != 0 && year % 4 == 0) {
+                days = 366;
+            } else if ( year % 100 == 0 || year % 400 != 0 && year % 100 != 0 && year % 4 != 0) {
+                days = 365;
+            }
+            if (days == day){
+            count++;
+            }
+            else {
+                System.out.println("Неправильно! В этом году " + days + " дней!");
+                System.out.println("Набрано очков: " + count);
+            }
+        }
+    }
+}
